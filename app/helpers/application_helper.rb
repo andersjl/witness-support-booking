64d2b333c274630c5e31291947570ec5e4602abb
Anders Lindeberg <anders.lindeberg@telia.com>
2013-01-12 21:34:41 +0100

    DRY static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def full_title( page_title)
+    base_title = "Bokning av vittnesstöd"
+    page_title.empty? ? base_title : "#{ base_title} | #{ page_title}"
+  end
+
 end
