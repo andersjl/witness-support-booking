d8babdfa9af181e334785ef70d7edebdfde9c7f9
Anders Lindeberg <anders.lindeberg@telia.com>
2013-02-02 19:02:58 +0100

    most of court_days/index to helpers

--- app/helpers/court_days_helper.rb
+++ app/helpers/court_days_helper.rb
@@ -1,15 +1,74 @@
 
 module CourtDaysHelper
 
+  def render_day( court_day, session_span, notes_span, at_bottom)
+    court_day_class = [ "court-day"]
+    court_day_class << "last" if at_bottom
+    court_day_id = "court-day-#{ court_day.date}"
+    bs_day = bootstrap_row( court_day_class, court_day_id)
+    bs_day << bootstrap_col( 1, "weekday", nil, weekday( court_day.date))
+    bs_day << bootstrap_col( 1, nil, nil, court_day.date)
+    edit_col = bootstrap_col( 2 * session_span + notes_span, nil, nil)
+    if current_user.admin?
+      edit_col << bs_admin_row( court_day, session_span, notes_span)
+    end
+    edit_col << bs_user_row( court_day, session_span, notes_span)
+    bs_day << edit_col
+    bs_day.to_s
+  end
+
+  def bs_admin_row( court_day, session_span, notes_span)
+    row = bootstrap_row( nil, nil)
+    [ "morning", "afternoon"].each do |session|
+      offered = court_day.send( session)
+      row << bootstrap_col( session_span, nil, nil, "#{ offered} totalt")
+    end
+    row << bootstrap_col( notes_span, nil, nil, h( court_day.notes
+                                               ).gsub( "\n", "<br/>"))
+    row
+  end
+
+  def bs_user_row( court_day, session_span, notes_span)
+    offset = 0
+    row = bootstrap_row( nil, nil)
+    [ "morning", "afternoon"].each do |session|
+      col = bs_session( court_day, session, offset, session_span)
+      if col
+        row << col
+        offset = 0
+      else
+        offset += session_span
+      end
+    end
+    unless current_user.admin?
+      row << bootstrap_col( notes_span, offset_to_class( offset), nil,
+                    h( court_day.notes).gsub( "\n", "<br/>").html_safe)
+    end
+    row
+  end
+
+  def bs_session( court_day, session, offset, session_span)
+    offered = court_day.send( session)
+    if offered > 0
+      col = bootstrap_col( session_span, offset_to_class( offset), nil)
+      taken = court_day.send( session + "_taken")
+      if offered - taken > 0
+        col << bootstrap_row( nil, nil, 
+                 bootstrap_col( session_span, nil, nil,
+                   "#{ offered - taken} kvar att boka"))
+      end
+      taken.times do
+        col << bootstrap_row( nil, nil,
+                 bootstrap_col( session_span, nil, nil,
+                   "NN"))
+      end
+      col
+    end
+  end
+
   def offset_to_class( offset)
     "offset#{ offset}" if offset > 0
   end
-
-=begin
-  def offset_n_span_to_class( offset, span)
-    (offset > 0 ? "offset#{ offset} " : "") + "span#{ span}"
-  end
-=end
 
   class ::String
     def indent
@@ -17,41 +76,46 @@ module CourtDaysHelper
     end
   end
 
-  class BootstrapScaffold
-
-    attr_reader :html
+  class BootstrapScaffold < Array
 
     def to_s
-      @html
+      result = @preamble
+      each{ |component| result += component.to_s.indent}
+      result + "\n</div>"
     end
 
     def initialize( content, given_class, html_class, html_id)
-      @html =  %Q$\n<div class="#{ given_class}$
+      @preamble =  %Q$\n<div class="#{ given_class}$
       if html_class
         html_class = html_class.flatten.join( " ") if html_class.is_a?( Array)
-        @html += %Q$ #{ html_class}$
+        @preamble += %Q$ #{ html_class}$
       end
-      @html += %Q$"$
+      @preamble += %Q$"$
       if html_id
-        @html += %Q$ id="#{ html_id}"$
+        @preamble += %Q$ id="#{ html_id}"$
       end
-      @html += %Q$>$
-      if content.is_a? Array
-        content.flatten.each{ |part| @html += part.to_s.indent}
-      else
-        @html += content.to_s.indent
-      end
-      @html += %Q$\n</div>$
+      @preamble += %Q$>$
+      content.each{ |part| append part}
     end
   end
 
   class BootstrapRow < BootstrapScaffold
-    def initialize( html_class, html_id, *columns)
+    def initialize( columns, html_class, html_id)
       super columns, "row", html_class, html_id
     end
   end
-  def bootstrap_row( *args)
-    BootstrapRow.new( *args)
+  def bootstrap_row( html_class, html_id, *cols)
+    BootstrapRow.new( cols, html_class, html_id)
+  end
+
+  class BootstrapCol < BootstrapScaffold
+    def initialize( content, span, html_class, html_id)
+      span = "span#{ span}" unless span.to_s[ 0, 4] == "span"
+      super span, html_class, html_id, content
+    end
+  end
+  def bootstrap_col( span, html_class, html_id, *content)
+    BootstrapCol.new( content, span, html_class, html_id)
   end
 
   class BootstrapCol < BootstrapScaffold
@@ -63,100 +127,5 @@ module CourtDaysHelper
   def bootstrap_col( *args)
     BootstrapCol.new( *args)
   end
-
-=begin
-  def td_rowspan( rows, content = nil)
-    html = "<td"
-    html += " rowspan=\"#{ rows}\"" if rows > 1
-    html + ">#{ content}</td>"
-  end
-
-  def court_day_rows( court_day)
-    [ 1, court_day.morning, court_day.afternoon].max
-  end
-
-  def morning_taken( court_day)
-    rand( court_day.morning + 1)  # stub
-  end
-
-  def afternoon_taken( court_day)
-    rand( court_day.afternoon + 1)  # stub
-  end
-
-# usage <%= court_days_table( @court_days).html_safe %>
-  def court_days_table( court_days)
-    html = ""
-    html += %Q$
-          <table class="table table-bordered table-condensed">$
-    html += %Q$
-            <thead><tr>
-              <th></th>
-              <th>Datum</th>
-              <th>Förmiddag</th>
-              <th>Eftermiddag</th>
-              <th>Noteringar</th>
-            </tr><thead>$
-  # html += "<p>#{ court_days.count}</p>"
-  # html += %Q$
-  #       <ul>$
-    court_days.each do |court_day|
-      morning_taken = rand( court_day.morning + 1)  # stub
-      afternoon_taken = rand( court_day.afternoon + 1)  # stub
-      court_day_rows = [ 1, court_day.morning, court_day.afternoon].max
-      1.upto( court_day_rows) do |row|
-        html += %Q$
-            <tr>$
-        if row == 1
-          html += %Q$
-              #{ td_rowspan( court_day_rows, weekday( court_day.date))}
-              #{ td_rowspan( court_day_rows, court_day.date)}$
-        end
-        if row <= morning_taken
-          html += %Q$
-              <td>NN</td>$  # stub
-        else
-          html += %Q$
-              #{ unbooked_row( row, morning_taken, court_day.morning,
-                               court_day_rows)}$
-        end
-        if row <= afternoon_taken
-          html += %Q$
-              <td>NN</td>$  # stub
-        else
-          html += %Q$
-              #{ unbooked_row( row, afternoon_taken, court_day.afternoon,
-                               court_day_rows)}$
-        end
-        if row == 1
-          html += %Q$
-              #{ td_rowspan( court_day_rows, court_day.notes)}$
-        end
-        html += %Q$
-            </tr>$
-      end
-    #   html += %Q$
-    #       <li>#{ court_day.date}, #{ court_day.morning}, #{ court_day.afternoon}, #{ court_day.notes}</li>$
-    end
-    html += %Q$
-          </table>$
-  # html += %Q$
-  #       </ul>$
-    html
-  end
-
-  def unbooked_row( row, taken, sessions, max)
-    if row <= sessions
-      if row == taken + 1
-        "<td>#{ sessions - taken} kvar att boka</td>"
-      else
-        "<td></td>"
-      end
-    elsif row == sessions + 1
-      td_rowspan( max - row + 1, "")
-    else
-      ""
-    end
-  end
-=end
 end
 
