aaccca01ff189cfa61597ab1b24b5bf6418844a0
Anders Lindeberg <anders.lindeberg@telia.com>
2013-02-23 21:36:34 +0100

    secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 secret = ENV[ "WITNESS_SUPPORT_BOOKING_SECRET"].to_s
 if secret.length < 30
-  raise "Secret token cannot be loaded"
+  raise "Secret token #{ secret.inspect} cannot be loaded"
 else
   WitnessSupportBooking::Application.config.secret_token = secret
 end
