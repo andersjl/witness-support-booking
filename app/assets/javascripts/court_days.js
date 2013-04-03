01197c7e61e3bce268613f706683949025f2489b
Anders Lindeberg <anders.lindeberg@telia.com>
2013-04-03 15:23:46 +0200

    english datepicker

--- app/assets/javascripts/court_days.js
+++ app/assets/javascripts/court_days.js
@@ -1,9 +1,4 @@
 $(function() {
-  $( ".datepicker" ).datepicker({
-    firstDay: 1, dateFormat: "yy-mm-dd",
-    dayNamesMin: [ "So", "Ma", "Ti", "On", "To", "Fr", "Lo" ],
-    monthNames: [
-      "Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli",
-      "Augusti", "September", "Oktober", "November", "December" ]});
+  $( ".datepicker" ).datepicker({ firstDay: 1, dateFormat: "yy-mm-dd" });
 });
 
