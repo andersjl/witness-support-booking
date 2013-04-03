db4d341a394e8c41145d3f4e73f3f5550dce15aa
Anders Lindeberg <anders.lindeberg@telia.com>
2013-04-03 09:54:06 +0200

    only 7-bit ascii in .js

--- app/assets/javascripts/court_days.js
+++ app/assets/javascripts/court_days.js
@@ -1,7 +1,7 @@
 $(function() {
   $( ".datepicker" ).datepicker({
     firstDay: 1, dateFormat: "yy-mm-dd",
-    dayNamesMin: [ "Sö", "Må", "Ti", "On", "To", "Fr", "Lö" ],
+    dayNamesMin: [ "So", "Ma", "Ti", "On", "To", "Fr", "Lo" ],
     monthNames: [
       "Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli",
       "Augusti", "September", "Oktober", "November", "December" ]});
