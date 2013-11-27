cb9bcf22085eb6ec3ef95ae7f901f4370beb0c8a
Anders Lindeberg <anders.lindeberg@telia.com>
2013-11-27 20:21:26 +0100

    Ruby 2.0.0

--- config/boot.rb
+++ config/boot.rb
@@ -6,5 +6,5 @@ ENV[ 'BUNDLE_GEMFILE'] ||= File.expand_path( '../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?( ENV[ 'BUNDLE_GEMFILE'])
 
-YAML::ENGINE.yamler = 'syck'
+YAML::ENGINE.yamler = 'psych'
 
